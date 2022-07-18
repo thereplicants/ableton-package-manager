3ab48c3f09811311d89cb8ab62d4f0e91760cf0a
Nathan Ward <ward@digivine.com>
2022-07-17 23:08:46 -0700

    Enable use of npm-style GitHub shorthand URLs

--- source/Install.tsx
+++ source/Install.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Text } from 'ink';
+import hostedGitInfo from 'hosted-git-info';
 import helpText from './helpText';
 
 const Install: FC<{ input?: string[] }> = ({ input }) => {
@@ -8,9 +9,18 @@ const Install: FC<{ input?: string[] }> = ({ input }) => {
     return <Text>{helpText}</Text>;
   }
 
+  const gitUrl = hostedGitInfo
+    .fromUrl(inputUrl)
+    ?.https()
+    .replace(/^git\+/, '');
+  if (!gitUrl) {
+    const error = 'Error: Could not find git repo to clone';
+    return <Text color="red">{error}</Text>;
+  }
+
   return (
     <>
-      <Text>Install {inputUrl}</Text>
+      <Text>Install {gitUrl}</Text>
     </>
   );
 };
